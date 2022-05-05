39129e092eb5c81854779e795cdeba07cbc57e8e
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2022-05-04 21:37:11 -0400

    we are POSTING git add .

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,13 +1,23 @@
 class TestsController < ApplicationController
-  def new
-    @test = Test.new
-  end
+
+  skip_before_action :verify_authenticity_token
 
   def show
     @test = Test.find(params[:id])
     render json: @test
   end
 
+  def index
+    @tests = Test.all
+    render json: @tests
+  end
+
   def create
+    @test = Test.create(test_params)
+    render json: @test
+  end
+
+  def test_params
+    params.require(:test).permit(:input_string, :output_string)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'tests/new'
-  get 'tests/show'
-  # get 'tests/create'
-  post 'tests/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tests 
+
 end
