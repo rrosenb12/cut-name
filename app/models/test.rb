d2cac5f692d99fb11fdf4f0b67fe8363bd48f288
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2022-05-05 20:30:20 -0400

    moving data manipulation log from controller to model

--- app/models/test.rb
+++ app/models/test.rb
@@ -15,4 +15,23 @@ class Test < ApplicationRecord
         #@final_output_string = final_output_string_array.join("")
     #end
 
+    def input_string=(input_string)
+        super(input_string)
+        self.output_string = generate_output(input_string)
+    end
+
+    private
+
+    def generate_output(input_string)
+      new_output_string = input_string.split("")
+      final_output_string_array = []
+      counter = 1
+      new_output_string.each do |letter|
+        if counter % 3 == 0
+          final_output_string_array.append(letter)
+        end      
+        counter = counter + 1
+      end
+      final_output_string_array.join("")
+    end
 end
